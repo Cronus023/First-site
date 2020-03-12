710f3ee6128a68302b5604fbfb39e20b91bf2e77
Cronus023 <anlrey.klochko@mail.ru>
2020-03-12 17:16:54 +0300

    fff

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ require('dotenv').config()
 app.use(express.json({extended:true}))
 app.use('/api/auth',require('./routes/auth.routes'))
 
-/*if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
-}*/
+}
 
 async function start() {
     try {
---
+++ client/build/precache-manifest.aef2df8853c989905d966910ae21012a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6dd77fe9dd0f8c8a2c624fc3eb7eb6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65dfad45d9b96e060a8d",
+    "url": "/static/css/2.41c7419c.chunk.css"
+  },
+  {
+    "revision": "0e2ead6170e6fd191d2b",
+    "url": "/static/css/main.723120b7.chunk.css"
+  },
+  {
+    "revision": "65dfad45d9b96e060a8d",
+    "url": "/static/js/2.c32d5a6c.chunk.js"
+  },
+  {
+    "revision": "503a35a7784ce5bd12e60cfc0731448a",
+    "url": "/static/js/2.c32d5a6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e2ead6170e6fd191d2b",
+    "url": "/static/js/main.a8cf8cec.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  }
+]);
